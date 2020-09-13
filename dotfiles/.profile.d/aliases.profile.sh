c6b52885b5a86638c13a01e4fdb51c5b595227c2
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-13 23:18:08 +0200

    Add keyd-watch alias; Watch keyboardmap service output

--- dotfiles/.profile.d/aliases.profile.sh
+++ dotfiles/.profile.d/aliases.profile.sh
@@ -7,3 +7,10 @@ alias keyd-start="sudo service keyboardmap start"
 alias keyd-stop="sudo service keyboardmap stop"
 alias keyd-restart="sudo service keyboardmap restart"
 alias keyd-status="sudo service keyboardmap status"
+
+keyd-watch() {
+    sudo service keyboardmap stop
+    sleep 1
+    sudo xkeysnail /etc/systemd/system/keyboardmap.service.d/xkeysnail.config.py
+    sudo service keyboardmap start
+}
