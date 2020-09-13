7b0d839e58d74bf7cc275d1f34f44e6afef76d87
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-13 03:47:22 +0200

    Add lm-sensors config

--- sensors/setup.sh
+++ sensors/setup.sh
@@ -1,2 +1,14 @@
+sudo apt install sensors
+sudo apt install inxi
+
+cd $(dirname $0)
+
+for f in ./sensors.d/*; do sudo ln -s $f /etc/sensors.d/; done
+
+sudo modprobe it87 force_id="0x8620"
+sensors-detect --auto
 sudo ln -s "`dirname $0`/probe.it87.conf" /etc/modprobe.d/it87.conf
 sudo ln -s "`dirname $0`/load.it87.conf" /etc/modules-load.d/it87.conf
+
+cd -
+
