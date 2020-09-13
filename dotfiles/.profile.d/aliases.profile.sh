84c80cf953a3177685c5f48577c7d986e100daf7
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-13 03:55:45 +0200

    Add wsens and winxi aliases
    watch sensors and watch winxi respectivly. With -n 1, and --color for inxi

--- dotfiles/.profile.d/aliases.profile.sh
+++ dotfiles/.profile.d/aliases.profile.sh
@@ -1,2 +1,4 @@
 alias scode="sudo code --user-data-dir=/home/sigsve/.vscode $@"
 alias reboot-bios="sudo systemctl reboot --firmware-setup"
+alias wsens="watch -n 1 sensors"
+alias winxi="watch --color -n 1 inxi -F"
