130fde3ccc0acdc1d08ecb30fc57107cdd46ba5f
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-13 03:53:53 +0200

    Add .profile dotfile for path mod. Add snap/bin to path

--- sensors/setup.sh
+++ sensors/setup.sh
@@ -3,7 +3,7 @@ sudo apt install inxi
 
 cd $(dirname $0)
 
-for f in ./sensors.d/*; do sudo ln -s $f /etc/sensors.d/; done
+for f in ./sensors.d/*; do sudo ln -s "`pwd`/$f" /etc/sensors.d/; done
 
 sudo modprobe it87 force_id="0x8620"
 sensors-detect --auto
