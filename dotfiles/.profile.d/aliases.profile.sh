31e0a74659fd4ffc30f0e9e134a08f09056cfcac
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-13 19:57:02 +0200

    Add alias for keyboardmap service start/stop/status/restart

--- dotfiles/.profile.d/aliases.profile.sh
+++ dotfiles/.profile.d/aliases.profile.sh
@@ -2,3 +2,8 @@ alias scode="sudo code --user-data-dir=/home/sigsve/.vscode $@"
 alias reboot-bios="sudo systemctl reboot --firmware-setup"
 alias wsens="watch -n 1 sensors"
 alias winxi="watch --color -n 1 inxi -F"
+
+alias keyd-start="sudo service keyboardmap start"
+alias keyd-stop="sudo service keyboardmap stop"
+alias keyd-restart="sudo service keyboardmap restart"
+alias keyd-status="sudo service keyboardmap status"
