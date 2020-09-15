17b34e28356b3e9609ae3613fa138406a8f5f047
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-15 22:48:43 +0200

    Add startup application so .startup.d script are actually ran
    xhost + is then handled by these scripts. So we no longer need to try to do
    that in the service

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -2,10 +2,14 @@
 for f in ./.ssh/*; do ln -s $(pwd)/dotfiles/$f ~/.ssh; done
 
 ## -- STARTUP.D -- ##
-ln -s $(pwd)/dotfiles/.startup.d ~/
+ln -s $(pwd)/dotfiles/.startup.d ~/ # Initiated by the startup.sh.desktop script in ~/.config/autostart
 
 ## -- XKB.d -- ##
 ln -s $(pwd)/dotfiles/.xkb.d ~/
+
+## -- .config/autostart -- ##
+# Starts the .startup scripts
+ln -s $(pwd)/dotfiles/.config/autostart/startup.sh.desktop ~/.config/autostart/
 
 ## -- PROFILE.D -- ##
 # Run all .profile.d .sh files on .bashrc
