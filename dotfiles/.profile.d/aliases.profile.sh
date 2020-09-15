bb068b9f63a87a4eb82aa60e51cf2b685d588fb5
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-15 16:29:42 +0200

    Add sensor chip to wsens alias
    So we dont have to all of them

--- dotfiles/.profile.d/aliases.profile.sh
+++ dotfiles/.profile.d/aliases.profile.sh
@@ -1,6 +1,6 @@
 alias scode="sudo code --user-data-dir=/home/sigsve/.vscode $@"
 alias reboot-bios="sudo systemctl reboot --firmware-setup"
-alias wsens="watch -n 1 sensors"
+alias wsens="watch -n 1 sensors \"it8620-isa-0a60\""
 alias winxi="watch --color -n 1 inxi -F"
 
 alias keyd-start="sudo service keyboardmap start"
